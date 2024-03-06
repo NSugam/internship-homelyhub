96a4d04bc0c8cdbd0a1f900fe54cad34b0132a39
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-06 20:33:59 +0530

    Updated Modals

--- frontend/src/Components/PropertyDetails/Modal.js
+++ frontend/src/Components/PropertyDetails/Modal.js
@@ -1,28 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-import "../../CSS/Modal.css"
+import "../../CSS/Modal.css";
 
 const Modal = ({ images, onClose }) => {
     useEffect(() => {
         document.body.style.overflow = "hidden";
         return () => {
-            document.body.style.overflow = "visible"
-        }
-    }, [])
+            document.body.style.overflow = "visible";
+        };
+    }, []);
+
     return (
-        <div className='modal-backdrop'>
-            <div className='modal-content'>
-                <button className='close-button' onClick={onClose}>
+        <div className="modal-backdrop">
+            <div className="modal-content">
+                <button className="close-button" onClick={onClose}>
                     <span>&times;</span>
                 </button>
-                <div className='modal-image-container'>
+                <div className="modal-image-container">
                     {images.map((image, index) => (
-                        <img key={index} src={image.url} alt={`Image ${index + 1} `} />
+                        <img key={index} src={image.url} alt={`Image ${index + 1}`} />
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Modal
+Modal.propTypes = {
+    images: PropTypes.array.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
--- frontend/src/Components/PropertyDetails/PropertyDetails.js
+++ frontend/src/Components/PropertyDetails/PropertyDetails.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from 'react-router-dom'
 import { getPropertyDetails } from '../../Store/PropertyDetails/propertyDetails-action'
 import PropertyImg from './PropertyImg'
+import PropertyAmenities from './PropertyAmenities'
 
 const PropertyDetails = () => {
     const dispatch = useDispatch();
@@ -15,7 +16,7 @@ const PropertyDetails = () => {
         dispatch(getPropertyDetails(id))
     }, [dispatch, id])
 
-    const {propertyName, address, images} = propertydetails;
+    const { propertyName, address, images, description, maximumGuest, amenities } = propertydetails;
 
     return (
         <div className='property-container'>
@@ -27,7 +28,20 @@ const PropertyDetails = () => {
                             <span className='material-symbols-outlined'>house</span>
                             <span className='location'>{`${address.area}, ${address.city}, ${address.pincode}, ${address.state}`}</span>
                         </h6>
-                        <PropertyImg images={images}/>
+                        <PropertyImg images={images} />
+                        <div className='middle-container row'>
+                            <div className='des-and-amenities col-md-8 col-sm-12 col-12'>
+                                <h2 className='property-description-header'>Description
+                                    <p className='property-description'>
+                                        {description}<br /><br />
+                                        Max number of Guests: {maximumGuest}<br/><br/>
+                                    </p>
+                                    <hr/>
+                                    <PropertyAmenities amenities={amenities}/>
+
+                                </h2>
+                            </div>
+                        </div>
                     </>
                 )
             }
