a31f5eb76702ee72f97c4456a4c03e560c1f831a
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-06 19:35:30 +0530

    Updated images

--- frontend/src/Components/PropertyDetails/PropertyImg.js
+++ frontend/src/Components/PropertyDetails/PropertyImg.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const PropertyImg = () => {
+const PropertyImg = ({ images }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleShowAllPhotos = () => {
+    setIsModalOpen(true)
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <div>
-      
+    <><div className='property-img-container'>
+      <div className='img-item first-image'>
+        <img className='images' src={images[0].url} alt="property-1"/>
+      </div>
+
+      {images.slice(1,5).map((image, index)=> {
+        <div key={index} className='img-item'>
+          <img className='images' src={image.url} alt={`property-${index+2}`}/>
+          </div>
+      })}
+
     </div>
+
+    </>
   )
 }
 
