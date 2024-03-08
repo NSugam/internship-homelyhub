7e1f1556d4c9cffb9a01e499ff17094e64c0da48
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-08 19:45:50 +0530

    Update user-slice.js

--- frontend/src/Store/User/user-slice.js
+++ frontend/src/Store/User/user-slice.js
@@ -17,6 +17,45 @@ const userSlice = createSlice({
             state.user = action.payload
             state.isAuthenticated = true
             state.loading = false
+        },
+        getLoginRequest(state) {
+            state.loading = true
+        },
+        getLoginDetails(state, action) {
+            state.user = action.payload
+            state.isAuthenticated = true
+            state.loading = false 
+        },
+        getError(state, action) {
+            state.errors = action.payload
+            state.loading = false 
+        },
+        getCurrentUserRequest(state) {
+            state.loading = true
+        },
+        getUpdateUserRequest(state) {
+            state.loading = true
+        },
+        getCurrentUser(state, action) {
+            state.user = action.payload
+            state.isAuthenticated = true
+            state.loading = false
+        },
+        getLogoutRequest(state) {
+            state.loading = true
+        },
+        getLogout(state, action) {
+            state.user = action.payload
+            state.isAuthenticated = false
+            state.loading = true
+        },
+        getPasswordRequest(state){
+            state.loading=true
+        },
+        getPasswordSuccess(state, action){
+            state.success=action.payload
+            state.loading=false
         }
+        
     }
 })
