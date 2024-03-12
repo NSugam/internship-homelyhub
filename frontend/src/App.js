d5de6c428e2d826b1e1c64fd067d3b75f61f89b1
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-12 21:13:48 +0530

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
-import Main from './Components/Home/Main';
-import Login from './Components/User/Login';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import PropertyList from './Components/Home/PropertyList';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import PropertyDetails from './Components/PropertyDetails/PropertyDetails';
+import PropertyList from './Components/Home/PropertyList';
 import { Flip, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { currentUser } from './Store/User/user-action';
 import { userActions } from './Store/User/user-slice';
+import Signup from './Components/User/Signup';
+import Profile from './Components/User/Profile';
+import 'react-toastify/dist/ReactToastify.css'
+import Main from './Components/Home/Main';
+import Login from './Components/User/Login';
+import { useEffect } from 'react';
+import './App.css';
 
 
 function App() {
@@ -32,14 +29,17 @@ function App() {
     createRoutesFromElements(
       <Route path='/' element={<Main />} exact >
         <Route id='home' index element={<PropertyList />} exact />
-        <Route path="propertylist/:id" id='PropertyDetails' element={<PropertyDetails />} exact />
-        <Route path="login" id='login' element={<Login />} exact />
+        <Route id='PropertyDetails' path="propertylist/:id" element={<PropertyDetails />} exact />
+        <Route id='login' path="login"  element={<Login />} exact />
+        <Route id='signup' path="signup"  element={<Signup />} exact />
+        <Route id='profile' path="profile"  element={<Profile />} exact />
       </Route>
     )
   );
   return (
     <div className='app'>
       <RouterProvider router={router} />
+      <ToastContainer position='bottom-center' autoClose={3000} draggable={true} transition={Flip}/>
 
     </div>
   );
