331002dce7ed53acbd89c22cf838523808ee4725
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-18 22:47:59 +0530

    Payment Updates

--- frontend/src/Components/Payment/Payment.js
+++ frontend/src/Components/Payment/Payment.js
@@ -1,11 +1,99 @@
-import React from 'react'
+import React from "react";
+import { CardNumberElement, CardExpiryElement, CardCvcElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { processPayment } from "../../Store/Payment/payment-actions";
+import "../../CSS/MyBookings.css";
 
 const Payment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const stripe = useStripe();
+  const elements = useElements();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { bookingId, propertyId } = useParams();
 
-export default Payment
+  const { isAuthenticated } = useSelector((state) => state.user);
+  const { checkinDate, checkoutDate, totalPrice, propertyName, address, maximumGuest, nights } = useSelector(
+    (state) => state.payment.paymentDetails
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    processPayment({
+      totalAmount: totalPrice,
+      stripe,
+      elements,
+      checkinDate,
+      checkoutDate,
+      propertyName,
+      address,
+      maximumGuest,
+      nights,
+      bookingId,
+      propertyId,
+      dispatch,
+      navigate,
+    });
+  };
+
+  return (
+    <div className="row wrapper">
+      <div className="col-10 col-lg-5">
+        {isAuthenticated ? (
+          <form className="shadow-lg" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="card_num_field">Card Number</label>
+              <CardNumberElement
+                id="card_num_field"
+                className="form-control"
+                options={{
+                  style: {
+                    base: {
+                      fontSize: "16px",
+                    },
+                  },
+                }}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="card_exp_field">Card Expiry</label>
+              <CardExpiryElement
+                id="card_exp_field"
+                className="form-control"
+                options={{
+                  style: {
+                    base: {
+                      fontSize: "16px",
+                    },
+                  },
+                }}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="card_cvc_field">Card CVC</label>
+              <CardCvcElement
+                id="card_cvc_field"
+                className="form-control"
+                options={{
+                  style: {
+                    base: {
+                      fontSize: "16px",
+                    },
+                  },
+                }}
+              />
+            </div>
+            <button type="submit" className="paymentbtn">
+              Pay - {totalPrice}
+            </button>
+          </form>
+        ) : (
+          <div>{navigate("/login")}</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
